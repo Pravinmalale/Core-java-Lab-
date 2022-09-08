d86a364c041cca716c0e7a5b769ded18f66b86ff
Pravinmalale <111423229+Pravinmalale@users.noreply.github.com>
2022-09-08 17:47:54 +0530

    Add files via upload

---
+++ 8 sep/JavaVersion.java
@@ -0,0 +1,13 @@
+
+
+class JavaVersion //declearing class
+{
+	public static void main(String Args[]) //calling main
+	{
+		System.out.println("Current version Of JVM- " +System.getProperty("java.version"));
+	} 
+	// end of main
+} 
+// end of class
+
+
